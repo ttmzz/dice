1fa947100adf7ccbe8788ab0f3abe3bf228ae5f0
ttmzz <112517972+ttmzz@users.noreply.github.com>
2023-03-07 18:59:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var num1 = Math.floor(Math.random() * 6) + 1;
-var dice = "images/dice" + num1 + ".png";
+var dice = "dice" + num1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", dice);
 
 var num2 = Math.floor(Math.random() * 6) + 1;
-var dice2 = "images/dice" + num2 + ".png";
+var dice2 = "dice" + num2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", dice2);
 
 if(num1 > num2) {
@@ -13,9 +13,3 @@ if(num1 > num2) {
 } else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
-
-
-/*🚩 Play 1 Wins!
-  Player 2 Wins! 🚩"
-  Draw!
-  */
